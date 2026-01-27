5333de1f444aeeec6e47ad4f1deb5d1f91e44d3c
bojandam <bojan.damjanovski1@gmail.com>
2026-01-27 20:09:34 +0100

    Created Spicies header file

---
+++ NEAT Implementation/species.hpp
@@ -0,0 +1,24 @@
+#ifndef NEAT_LIBRARY_SPECIES_H
+#define NEAT_LIBRARY_SPECIES_H
+
+#include"individual.hpp"
+namespace NEAT {
+    struct Species
+    {
+        Individual representitive;
+        std::vector<Individual> members;
+        int lifetime = 0;
+        void PrepareForNextGeneration(int newRepresentativeIndex = 0) {
+            representitive = std::move(members[newRepresentativeIndex]);
+            members.clear();
+        }
+        Species(Individual && representitive) :representitive(representitive) {
+            members.push_back(this->representitive);
+            lifetime++;
+        }
+    };
+}
+
+
+
+#endif
